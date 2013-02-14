2ae9a612d6435bddb5e9bcb3f64f8439a80db843
loyso <loyso.b@gmail.com>
2013-02-14 23:01:52 +0400

    Parse command line.

--- builder/builder.cpp
+++ builder/builder.cpp
@@ -1,9 +1,85 @@
-#include <stdio.h>
 #include <tchar.h>
 
+#include <iostream>
+#include <fstream>
+#include <boost/program_options.hpp>
 
-int _tmain(int argc, _TCHAR* argv[])
+#include <rollaball/rollaball.h>
+
+int ParseCommandLine( int argc, wchar_t** argv, rab::Options& options, rab::Config& config )
 {
+	namespace po = boost::program_options;
+
+	po::options_description command_line_options("Command line options");
+	command_line_options.add_options()
+		("help,H", "produce help message")
+		("src,S", po::value(&options.src), "path to new content folder")
+		("dst,D", po::value(&options.dst), "path to old content folder")
+		("tmp,T", po::value(&options.tmp)->default_value("./temp"), "path to temp folder")
+		("config,C", po::value(&options.config_file), "name of a configuration file.")
+		("src_ver", po::value(&options.src_ver), "a name for new version")
+		("dst_ver", po::value(&options.dst_ver), "a name for old version")
+		;
+
+	po::options_description config_file_options("Config file options");
+	config_file_options.add_options()
+		("dst_folders", po::value(&config.dst_folders), "destination folders to patch")
+		("dst_files", po::value(&config.dst_files), "destination files to patch")
+		("dst_ignore_folders", po::value(&config.dst_ignore_folders), "skip destination folders")
+		("dst_ignore_files", po::value(&config.dst_ignore_files), "skip destination files")
+		("pack_files_using", po::value(&config.pack_files_using), "choose pack method for source files: \"mask/method\"")
+		("dst_ignore_changed", po::value(&config.dst_ignore_changed), "destination files not to patch, if dst changed")
+		("dst_preserve_removed", po::value(&config.dst_preserve_removed), "destination files to preserve, if src removed")
+		("src_ignore_files", po::value(&config.src_ignore_files), "skip source files")
+		("src_file_limit", po::value(&config.src_file_limit)->default_value(0), "skip source files greater then the limit")		
+		("packed_extension", po::value(&config.packed_extension)->default_value("diff"), "extension for packed files")
+		;
+
+	po::variables_map vm;
+	po::store(po::wcommand_line_parser(argc, argv).options(command_line_options).run(), vm);
+	po::notify(vm);    
+
+	if (vm.count("help")) 
+	{
+		std::cout << command_line_options << "\n";
+		std::cout << config_file_options << "\n";
+	}
+
+	if( !options.config_file.empty() )
+	{
+		std::ifstream ifs(options.config_file.c_str());
+		if (!ifs)
+		{
+			std::cout << "can not open config file: " << options.config_file << "\n";
+			return 1;
+		}
+		else
+		{
+			po::store(po::parse_config_file(ifs, config_file_options), vm);
+			po::notify(vm);
+		}
+	}
+
 	return 0;
 }
 
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int result = 1;
+
+	rab::Options options;
+	rab::Config config;
+
+	try
+	{
+		result = ParseCommandLine( argc, argv, options, config );
+	}
+	catch(std::exception& e) {
+		std::cerr << e.what() << "\n";
+	}
+
+	if( result )
+		return result;
+
+	return 0;
+}
--- rollaball/rollaball.h
+++ rollaball/rollaball.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+namespace rab
+{
+	typedef std::vector< std::string > ConfigValues_t;
+
+	struct Options
+	{
+		std::string src, dst, tmp;
+		std::string src_ver, dst_ver;
+		std::string config_file;
+	};
+
+	struct Config
+	{
+		ConfigValues_t dst_folders;
+		ConfigValues_t dst_files;
+		ConfigValues_t dst_ignore_folders;
+		ConfigValues_t dst_ignore_files;
+		ConfigValues_t pack_files_using;
+		ConfigValues_t dst_ignore_changed;
+		ConfigValues_t dst_preserve_removed;
+		ConfigValues_t src_ignore_files;
+		int src_file_limit;
+		std::string packed_extension;
+	};
+};
+
